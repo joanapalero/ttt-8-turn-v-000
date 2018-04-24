6d095c20fa3c7e778a6944df8ccf0d8ac503e4ab
Joana Palero <joana.palero24@gmail.com>
2018-04-24 23:41:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,10 +4,11 @@
       input = gets.strip
       index = input_to_index(input)
       if valid_move?(board, index)
+        move(board, index)
+        display_board(board)
     else
       turn(board)
     end
-      display_board(board)
 end
 
 #display_board
