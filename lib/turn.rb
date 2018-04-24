411ea03b6187e6b2d87112982bc78ddd98e09e19
Joana Palero <joana.palero24@gmail.com>
2018-04-24 23:32:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,8 @@
           return FALSE
       end
     end
-    # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+    
+    
     def position_taken?(board, index)
         if (board[index] == " " || board[index] == "" || board[index] == NIL)
           return FALSE
